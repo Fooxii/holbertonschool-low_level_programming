b4183697c9b4d6b883beefed698984150e46999b
Fooxii <ironata07@gmail.com>
2025-10-15 11:32:23 -0400

    betty

--- hello_world/hello_world.c
+++ hello_world/hello_world.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-*main - Entry point
-*
-*printing "Hello, World!" to the standard output
-*/
+ *main - Entry point
+ *
+ *printing "Hello, World!" to the standard output
+ */
 int main(void)
 {
- /*your code goes here*/
+ //your code goes here
 printf("Hello, World!\n");
 return (0);
 }
