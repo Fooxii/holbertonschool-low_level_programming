1af655b1cd96541bcd0c5de030c29a9535f56f40
Fooxii <ironata07@gmail.com>
2025-10-16 10:12:54 -0400

    Added  script to 6-size.c

---
+++ 6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *prints string to the standard output
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+ /*your code goes here*/
+printf("Size of a char: %zu bytes \n", sizeof(char));
+printf("Size of an int: %zu bytes \n", sizeof(int));
+printf("Size of a long int: %zu bytes \n", sizeof(long int));
+printf("Size of a long long int: %zu bytes \n", sizeof(long long int));
+printf("Size of a float: %zu bytes \n", sizeof(float));
+}
