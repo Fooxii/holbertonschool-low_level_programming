070bcc785a1eeb39dd040ce3a155a38a3fc726e6
Fooxii <ironata07@gmail.com>
2025-10-16 09:31:51 -0400

    added script for 5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *prints string to the standard output
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+ /*your code goes here*/
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
