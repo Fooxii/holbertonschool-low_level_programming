07dc1d8fa8d9c93c6fcec687443ac6bcc1fe0888
Fooxii <ironata07@gmail.com>
2025-10-16 22:00:22 -0400

    Added script for 1-last_digit.c

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+printf("%d", n % 10);
+printf(" is");
+if (n % 10 > 5)
+printf(" and is greater than 5\n");
+else if (n % 10 < 6 && n != 0)
+printf(" and is less than 6 and not 0\n");
+else
+printf(" and is zero\n");
+return (0);
+}
