de451f176e10aaa1ab7ebe28e3c6e32d62a0c595
Fooxii <ironata07@gmail.com>
2025-10-17 20:28:05 -0400

    Added script for 5-print_numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char num;
+while (num = '0'; num <= '9'; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
