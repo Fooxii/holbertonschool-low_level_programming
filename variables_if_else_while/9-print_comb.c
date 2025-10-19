dae3996617d4d8c75f46d436faaf1fc994f1f4eb
Fooxii <ironata07@gmail.com>
2025-10-19 13:26:42 -0400

    Added script for 9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints all combinations from 00 to 99
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num = 0;
+
+while (num <= 99)
+{
+putchar((num / 10) + '0');
+putchar((num % 10) + '0');
+
+if (num != 99)
+{
+putchar(',');
+putchar(' ');
+}
+
+num++;
+}
+
+return 0;
+}
