49109058d3cf8161e220bdf233717c266041d69f
Fooxii <ironata07@gmail.com>
2025-10-19 18:22:12 -0400

    2nd attempt to fix script in 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,17 +8,14 @@
 
 int main(void)
 {
-my_putchar();
-{
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar('\n');
-}
+my_putchar('_');
+my_putchar('p');
+my_putchar('u');
+my_putchar('t');
+my_putchar('c');
+my_putchar('h');
+my_putchar('a');
+my_putchar('r');
+my_putchar('\n');
 return (0);
 }
