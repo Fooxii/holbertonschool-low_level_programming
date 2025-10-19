36070257ed3a62ed24ed948ca0be03c515d5e794
Fooxii <ironata07@gmail.com>
2025-10-19 18:49:51 -0400

    Added script for 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -8,13 +8,17 @@
 
 int main(void)
 {
+void print_alphabet(void);
+{
 char letter;
+
 
 for (letter = 'a'; letter <= 'z'; letter++)
 {
 _putchar(letter);
 }
-_putchar('\n');
 
+_putchar('\n');
 return (0);
+}
 }
