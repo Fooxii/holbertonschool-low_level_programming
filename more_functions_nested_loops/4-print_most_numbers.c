6d5d7f39a4da91fea213257ecc2a376b32ac79ce
Fooxii <ironata07@gmail.com>
2025-10-20 11:27:05 -0400

    fixed description for betty

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9,
- * followed by a new line. Do not print 2 and 4
+ * print_most_numbers - prints the numbers from 0 to 9
+ *                      except 2 and 4
  *
  * Return: void
  */
