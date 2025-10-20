cec6e0a13ebe621e1b3ec0f41f5b0f718d9184d7
Fooxii <ironata07@gmail.com>
2025-10-20 13:00:18 -0400

    Added script for 6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line in the terminal
+ * @n: number of times the character _ should be printed
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+int n;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+while (n > 0)
+{
+_putchar('_');
+n--;
+}
+}
+_putchar('\n');
+}
