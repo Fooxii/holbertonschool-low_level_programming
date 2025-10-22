5d5b297aa093a41e57c509e670fe12d06eaa19f5
Fooxii <ironata07@gmail.com>
2025-10-22 11:03:17 -0400

    added space using putchar

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -16,19 +16,23 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
+putchar(' ');
 }
 else if (i % 3 == 0)
 {
-printf("Fizz ");
+printf("Fizz");
+putchar(' ');
 }
 else if (i % 5 == 0)
 {
-printf("Buzz ");
+printf("Buzz");
+putchar(' ');
 }
 else
 {
-printf("%d ", i);
+printf("%d", i);
+putchar(' ');
 }
 }
 return (0);
