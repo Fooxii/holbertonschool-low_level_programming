38e078ed3d95b09526117259225f27024f4fb6ac
Fooxii <ironata07@gmail.com>
2025-10-22 11:05:18 -0400

    fix to having 1 extra character being the cause for the error

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@
 int main(void)
 {
 int i;
-for (i = 1; i <= 100; i++)
+for (i = 1; i <= 99; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
@@ -35,6 +35,6 @@ printf("%d", i);
 putchar(' ');
 }
 }
-putchar('\n');
+printf("Buzz\n");
 return (0);
 }
