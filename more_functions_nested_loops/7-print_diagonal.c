02582326fd043287fab32c48e2dbf9b2a9d5606a
Fooxii <ironata07@gmail.com>
2025-10-22 10:36:44 -0400

    3rd attempt to fix script in 7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -2,26 +2,31 @@
 
 /**
  * print_diagonal - prints a diagonal line on the terminal
- * @n: number of times the character \ should be printed
+ * @n: number of times the character '\' should be printed
  *
  * Return: void
  */
 
 void print_diagonal(int n)
 {
-while (n > 0)
-{
-int spaces = n - 1;
-while (spaces > 0)
-{
-_putchar(' ');
-spaces--;}
-_putchar('\\');
-_putchar('\n');
-n--;
-}
+int i, j;
 if (n <= 0)
 {
 _putchar('\n');
+return;
 }
+
+
+for (i = 0; i < n; i++)
+{
+
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+
+_putchar('\\');
+_putchar('\n');
+}
+
 }
