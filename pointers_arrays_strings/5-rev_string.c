809e3753494c7f4f178e8f45e0ff52aefd19ef25
Fooxii <ironata07@gmail.com>
2025-10-24 22:24:55 -0400

    Added script in 5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ * rev_string - function that reverses a string
+ *
+ * return void
+ */
+
+void rev_string(char *s)
+{
+int length;
+
+
+while (*s != '\0')
+{
+s++;
+}
+
+for (length = s - 1; length > 0; length--)
+{
+_putchar(*s);
+s--;
+}
+}
