c1b934e6b053632970b3f2104fbce81435f53945
Fooxii <ironata07@gmail.com>
2025-10-24 21:57:39 -0400

    6th attempt to fix script in 4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,9 @@
 #include "main.h"
 
-
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: string to be printed
+ */
 
 void print_rev(char *s)
 {
@@ -16,7 +19,7 @@ s++;
 
 s--;
 
-for (length = len; length >= 0; length--)
+for (length = len; length > 0; length--)
 {
 _putchar(*s);
 s--;
