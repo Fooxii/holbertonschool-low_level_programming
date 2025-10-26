db5db93f2089ef47f5dd59effc6eba663639b219
Fooxii <ironata07@gmail.com>
2025-10-26 15:50:12 -0400

    4th attempt to fix script in 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,29 +10,10 @@
 int _strcmp(char *s1, char *s2)
 {
 
-while (*s1 != '\0')
+while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
 {
 s1++;
-}
-
-while (*s2 != '\0')
-{
 s2++;
-
 }
-
-if (*s1 < *s2)
-{
-return (-1);
-}
-
-else if (*s1 > *s2)
-{
-return (1);
-}
-
-else
-{
-return (0);
-}
+return (*s1 - *s2);
 }
