f131fa68af30d15fe0836635eec7686cc6cb1231
Fooxii <ironata07@gmail.com>
2025-10-26 12:18:29 -0400

    Added script in 0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strcat - merges two strings
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ * Return: dest string merged with src string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+char *p = dest;
+
+
+while (*p != '\0')
+{
+p++;
+}
+
+
+while (*src != '\0')
+{
+*p = *src;
+p++;
+src++;
+}
+
+
+*p = '\0';
+
+return dest;
+}
