45ad6a6422b35643ecbd5463cc0df1ea43db3b8b
Fooxii <ironata07@gmail.com>
2025-10-25 21:37:13 -0400

    Added script in 100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _atoi - converts string to an interger
+ * @s: string to be converted
+ * Return: the integer found in the string or 0 if no numbers are present
+ */
+int _atoi(char *s)
+{
+int sign = 1;
+int num = 0;
+int found = 0;
+
+while (*s)
+{
+if (*s == '-')
+sign *= -1;
+else if (*s == '+')
+;
+else if (*s >= '0' && *s <= '9')
+{
+num = num * 10 + (*s - '0');
+found = 1;
+}
+else if (found)
+{
+break;
+}
+
+s++;
+}
+
+return (num *sign);
+}
