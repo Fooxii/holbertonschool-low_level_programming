137448ce7733d72a1c69f9b56815440f40315a6d
Fooxii <ironata07@gmail.com>
2025-10-26 14:13:47 -0400

    Added script in 2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncpy - function copies string from src to dest
+ * @dest: pointer to destination of copied string
+ * @src: pointer to string to be copied
+ * @n: number of char to be copied
+ * Return: pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+char *copy = dest;
+while (*src != '\0' && n > 0)
+{
+*copy = *src;
+src++;
+copy++;
+n--;
+}
+
+if (n > 0)
+{
+*copy = '\0';
+copy++;
+n--;
+}
+
+if (n <= 0)
+{
+return (dest);
+}
+
+}
