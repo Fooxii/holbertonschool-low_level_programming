bcd046aeafad8a3a269d28b48177e8a691a91f5d
Fooxii <ironata07@gmail.com>
2025-10-25 20:02:44 -0400

    Added script in 9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcpy - function copies string from src over to dest
+ * @src: stores string being copied
+ * @dest: pointer to the destination of copied string
+ * return - returns pointer dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+char *temp = dest;
+while (*src != '\0')
+{
+*temp = *src;
+temp++;
+src++;
+}
+*temp = '\0';
+_putchar('\n');
+return dest;
+}
