8e8879a4e3d18fd7eac867321336fa6d99f627de
Fooxii <ironata07@gmail.com>
2025-10-28 12:25:26 -0400

    updated main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -22,7 +22,7 @@ char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *);
 char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned in n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
