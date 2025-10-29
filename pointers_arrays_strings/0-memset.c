9f66ab47c749c8776a9f6f48842c4011a2873702
Fooxii <ironata07@gmail.com>
2025-10-28 22:09:30 -0400

    Added script in 0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _memset - function that fills memory with a constant byte
+ * @s: pointer to memory area
+ * @b: constant byte to fill memory with
+ * @n: number of bytes to fill
+ * Return: pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+char *p = s;
+if (n <= 0)
+{
+return (*s);
+}
+
+while (n > 0)
+{
+*p = b;
+p++;
+n--;
+}
+
+return (s);
+}
