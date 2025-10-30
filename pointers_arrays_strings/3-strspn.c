d8793cac8081d5e12f2002c05ef68893cdc32a61
Fooxii <ironata07@gmail.com>
2025-10-30 11:22:07 -0400

    5th attempt to fix script in 3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,10 +10,25 @@
 unsigned int _strspn(char *s, char *accept)
 {
 unsigned int count = 0;
-while (*s == *accept && *s != '\0')
+char *a;
+
+while (*s != '\0')
+{
+*a = *accept;
+while (*a != '\0')
+{
+if (*s == *a)
 {
 count++;
+break;
+}
+a++;
+}
+if (*a == '\0')
+return (count);
+
 s++;
 }
+
 return (count);
 }
