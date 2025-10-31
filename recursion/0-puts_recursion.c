0b60b893321eb7833120a74397109f1b745c5720
Fooxii <ironata07@gmail.com>
2025-10-31 14:04:34 -0400

    2nd attempt to fix script in 0-puts_recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -3,14 +3,14 @@
 /**
  * _puts_recursion - function that prints a string, followed by a new line
  * @s: string being printed
- * Return: void
  */
 
 void _puts_recursion(char *s)
 {
-char *a = (*s + (*s + 1));
 if (*a == '\0')
 {
-return (*s);
+char *a = (*s + (*s + 1));
+
+_putchar('\n');
 }
 }
