d83f4ef821096ca3b345d8cefee6960d630b5c50
Fooxii <ironata07@gmail.com>
2025-10-31 21:48:06 -0400

    Added script in 3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - function that returns the factorial of a given number
+ * @n: number for the factorial
+ * Return: returns factorial of n, 1 if n = 0, and -1 if n < 0
+ */
+
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+if (n == 0)
+{
+return (1);
+}
+else
+{
+return (factorial(n = (n - 1)));
+}
+}
