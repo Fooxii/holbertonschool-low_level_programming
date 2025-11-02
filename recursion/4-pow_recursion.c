267743dac1a35b7e68e1a17ab2b7ec4d9805ff97
Fooxii <ironata07@gmail.com>
2025-11-02 17:07:46 -0400

    betty

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _pow_recursion - function that returns the value of x raised to the power of y
+ * _pow_recursion - function that returns x raised to the power of y
  * @x: number
  * @y: exponent
- * Return: x raised to y, -1 if y < 0,and 1 if y = 0
+ * Return: x raised to the power of x, -1 if y < 0,and 1 if y = 0
  */
 
 int _pow_recursion(int x, int y)
