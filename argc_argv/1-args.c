905442d17679d8f4a1c8cf971e08132804000eec
Fooxii <ironata07@gmail.com>
2025-11-02 19:37:27 -0400

    Added scipt in 1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - function that prints the number of arguments passed into it
+ * @argc: number of arguments
+ * @argv: void
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+_putchar(argc);
+_putchar('\n');
+return (0);
+}
