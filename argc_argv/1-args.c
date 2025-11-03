704aca0768c6c7c4e6d2159c83c2466118209973
Fooxii <ironata07@gmail.com>
2025-11-02 21:28:12 -0400

    2nd attempt to fix script in 1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,17 +1,29 @@
 #include "main.h"
-#include <stdio.h>
+
+/**
+ * print_number - function that turns string of number/s into digits
+ * @n: number being converted from string to digit/s
+ */
+
+void print_number(int n)
+{
+if (n / 10)
+print_number(n / 10);
+_putchar((n % 10) + '0');
+}
 
 /**
  * main - function that prints the number of arguments passed into it
  * @argc: number of arguments
  * @argv: void
  * Return: 0
- */
+  */
 
 int main(int argc, char *argv[])
 {
-printf("%d", argc - 1);
 (void)argv;
+
+print_number(argc - 1);
 _putchar('\n');
 return (0);
 }
