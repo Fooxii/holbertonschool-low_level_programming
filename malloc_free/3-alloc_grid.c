bc7a743bb0eba60f4e314e6fbcae072c4bfa63be
Fooxii <ironata07@gmail.com>
2025-11-05 14:29:11 -0400

    Added script in 3-alloc_grid.c

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2D array of intergers
+ * @width: the width of the 2d array
+ * @height: the height of the 2d array
+ * Return: 2d array, NULL if width or height < 0 and if malloc fails
+ */
+
+int **alloc_grid(int width, int height)
+{
+int **arr2d;
+int h;
+int w;
+
+if (width <= 0 || height <= 0)
+{
+return ((void *)0);
+}
+
+arr2d = malloc(height * sizeof(int));
+if (arr2d == (void *)0)
+{
+return ((void *)0);
+}
+
+for (h = 0; h < height; h++)
+{
+arr2d[h] = malloc(width * sizeof(int));
+if (arr2d[h] == (void *)0)
+{
+return ((void *)0);
+}
+
+for (w = 0; w < width; w++)
+{
+arr2d[h][w] = 0;
+}
+}
+
+return (arr2d);
+}
