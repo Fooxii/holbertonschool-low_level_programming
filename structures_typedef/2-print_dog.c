958236007e373501027c60bfa5785223bb562643
Fooxii <ironata07@gmail.com>
2025-11-07 19:06:31 -0400

    Added script in 2-print_dog.c

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,33 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * print_dog - function that prints a struct dog
+ * @d: pointer to struct
+ */
+
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+{
+return;
+}
+
+if (d->name == (void *)0)
+{
+printf("Name: (nil)\n");
+}
+else
+printf("Name: %s\n", d->name);
+
+printf("Age: %f\n", d->age);
+
+if (d->owner == (void *)0)
+{
+printf("Owner: (nil)\n");
+}
+else
+{
+printf("Owner: %s\n", d->owner);
+}
+}
