6b3e91fcde01f26facbae7149a3087fea4fb0bf6
Fooxii <ironata07@gmail.com>
2025-11-07 13:17:54 -0400

    Added script in 3-array_range.c

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - function that creates an array of intergers
+ * @min: first number of array
+ * @max: last number of array
+ * Return: pointer to new array or NULL if min > max or malloc fails
+ */
+
+int *array_range(int min, int max)
+{
+int i;
+char *arr;
+int diff = (max - min);
+if (min > max)
+{
+return ((void *)0);
+}
+
+arr = malloc(diff * sizeof(int));
+if (arr == (void *)0)
+{
+return ((void *)0);
+}
+
+for (i = 0; diff > 0; i++)
+{
+arr[i] = min + i;
+diff--;
+}
+
+return (arr);
+}
