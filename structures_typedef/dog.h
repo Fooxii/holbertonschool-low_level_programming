1663800f85d3569ac9aa898642c581c4b8c26483
Fooxii <ironata07@gmail.com>
2025-11-07 17:02:52 -0400

    Added dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dog information
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /*DOG_H*/
