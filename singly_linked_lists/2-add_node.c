e5251cbe10fa24a7c1fa2efc34e472d48de6fa62
Fooxii <ironata07@gmail.com>
2025-11-25 16:02:49 -0400

    added library for malloc, and moved declarations to top

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * add_node - function that adds a new node at the beginning of a list_t list
@@ -11,23 +12,26 @@
 list_t *add_node(list_t **head, const char *str)
 {
 char *dupstr;
+list_t *newNode;
+unsigned int length;
+
 dupstr = strdup(str);
-int length = strlen(dupstr);
 if (dupstr == (void *)0)
 {
 return ((void *)0);
 }
+length = strlen(dupstr);
 
-list_t *newNode;
 newNode = malloc(sizeof(list_t));
 if (newNode == (void *)0)
 {
 return ((void *)0);
 }
+
 newNode->str = dupstr;
 newNode->len = length;
 newNode->next = *head;
-*head = &newNode;
+*head = newNode;
 
 return (newNode);
 }
