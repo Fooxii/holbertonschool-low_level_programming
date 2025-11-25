84401cfcaaf2ee06229a801c57a72daaf482f986
Fooxii <ironata07@gmail.com>
2025-11-25 14:28:58 -0400

    Added script to 2-add_node.c

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node - function that adds a new node at the beginning of a list_t list
+ * @head: start of list
+ * @str: string being printed
+ * Return: the address of the new element or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+char *dupstr;
+dupstr = strdup(str);
+int length = strlen(dupstr);
+if (dupstr == (void *)0)
+{
+return ((void *)0);
+}
+
+list_t newNode;
+newNode.str = dupstr;
+newNode.len = length;
+newNode.next = *head;
+*head = &newNode;
+
+return (&newNode);
+}
