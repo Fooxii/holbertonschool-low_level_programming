fa38e78c1b73948a7e8759f2d30c040264bba4b0
Fooxii <ironata07@gmail.com>
2025-11-25 13:40:33 -0400

    betty fix (comment over 80 char long)

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * list_len - function that returns the number of elements in a linked list_t list
+ * list_len - function that returns the number of elements in a linked list
  * @h: pointer to list
  * Return: number of elements in linked list
  */
