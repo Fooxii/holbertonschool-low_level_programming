4fc3304c7711a8cb615e639155caea6386343af6
Fooxii <ironata07@gmail.com>
2025-11-25 13:27:40 -0400

    Added script for 0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_list - function that prints all elements of a list_t list
+ * @h: pointer to list
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+int countNode = 0;
+while (h != (void *)0)
+{
+if (h->str == (void *)0)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%u] %s\n", h->len, h->str);
+}
+countNode++;
+h = h->next;
+}
+return (countNode);
+}
