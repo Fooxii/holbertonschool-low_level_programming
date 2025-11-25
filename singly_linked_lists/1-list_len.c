c5debb68c9867dbd2dc6dce324136ffddcf54cad
Fooxii <ironata07@gmail.com>
2025-11-25 13:39:22 -0400

    Added script to 1-list_len.c

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements in a linked list_t list
+ * @h: pointer to list
+ * Return: number of elements in linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+int countNode = 0;
+while (h != (void *)0)
+{
+countNode++;
+h = h->next;
+}
+return (countNode);
+}
