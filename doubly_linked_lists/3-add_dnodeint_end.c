83b00652dbe59a11382dfc7e10883de6a5a20555
Fooxii <ironata07@gmail.com>
2025-11-26 11:26:51 -0400

    typo in function prototypes description too

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_nodeint_end - function that adds a new node at the end of a list
+ * add_dnodeint_end - function that adds a new node at the end of a list
  * @head: pointer to the head of the list
  * @n: interger being added to the new node
  * Return: adress of the new element, or NULL if it failed
