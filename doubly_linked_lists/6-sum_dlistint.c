a738ddb4961696b417165c15d7162efcdf573bd6
Fooxii <ironata07@gmail.com>
2025-11-26 13:37:16 -0400

    Added script to 6-sum_dlistint.c

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_dlistint - function that returns the sum of all the data (n) of a list
+ * @head: pointer to the head of the list
+ * Return: sum of all the data (n) of a list
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+dlistint_t *temp = head;
+int sum = 0;
+
+while (temp != (void *)0);
+{
+sum += temp->n;
+temp = temp->next;
+}
+return (sum);
+}
