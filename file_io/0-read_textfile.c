8fc6d4340cbe82a1ecdf281be55050a18c916ce1
Fooxii <ironata07@gmail.com>
2025-11-30 19:50:44 -0400

    Brackets

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -17,11 +17,15 @@ ssize_t fd, bytes_read, bytes_written;
 char *buffer;
 
 if (filename == NULL)
+{
 return (0);
+}
 
 fd = open(filename, O_RDONLY);
 if (fd == -1)
+{
 return (0);
+}
 
 buffer = malloc(sizeof(char) * letters);
 if (buffer == NULL)
