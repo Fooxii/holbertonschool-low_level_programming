8700a2215bb1d9fdd033bb6e12de2d5e4ecc54b4
Fooxii <ironata07@gmail.com>
2025-12-01 19:21:05 -0400

    Added script to 0-hash_table_create.c

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that creates a hash table
+ * @size: size of the array
+ * Return: pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t *ht;
+unsigned long int i;
+ht = malloc(sizeof(hash_table_t));
+if (ht == (void *)0)
+{
+return ((void *)0);
+}
+ht->size = size;
+return (ht);
+}
