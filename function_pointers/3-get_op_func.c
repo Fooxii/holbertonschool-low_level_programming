24c3dfb8e08c81b88386cf438fd6ccb15a4b696b
Fooxii <ironata07@gmail.com>
2025-12-14 23:00:12 -0400

    Fixed script in 3-get_op_func.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -18,9 +18,17 @@ op_t ops[] = {
 };
 int i;
 
-i = 0;
+ i = 0;
 while (ops[i].op)
+{
 if (ops[i].op[0] == s[0] && s[1] == '\0')
-return (ops[i].f), i++;
+{
+return (ops[i].f);
+}
+else
+{
+i++;
+}
+}
 return (NULL);
 }
